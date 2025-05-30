875b31ec633b4ec54550fbacc8b76ada6e9939f9
suko19assign <suko19@student.bth.se>
2025-05-30 10:29:37 +0200

    bein

--- server.c
+++ server.c
@@ -1,12 +1,17 @@
+#define _POSIX_C_SOURCE 200809L
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <regex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-/* You will to add includes here */
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-
-
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
-  
-
-}
+#define PROTO_VERSION    "1.0"
+#define MAX_NICK_LEN     12
+#define MAX_MSG_BODY     255
+#define MAX_LINE         (6 + MAX_MSG_BODY) /* "MSG " + body + '\n' */
