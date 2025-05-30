4004ade0645374d31b4356128b35328d4c62688b
suko19assign <suko19@student.bth.se>
2025-05-30 10:27:06 +0200

    main imprint

--- client.c
+++ client.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-/* You will to add includes here */
-
-
-int main(int argc, char *argv[]){
-  
-  /* Do magic */
-  
-
-}
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s HOST:PORT NICK\n", argv[0]);
+        return EXIT_FAILURE;
+    }
